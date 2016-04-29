96da4c8ecc510810ec3c38e8938c7c3fef801089
Dash Peters <dpeters19@bayschoolsf.org>
2016-04-29 16:42:48 -0700

    Add script to combine length and highest number in sequence

---
+++ Length_Highest.py
@@ -0,0 +1,60 @@
+def highest(start, stop):
+    begin = start
+    dict_max = {}
+    while begin <= stop:
+        current = set()
+        number = begin
+        if begin == 1:
+            number = 2
+        while number >= 1:
+            if number == 1:
+                max_num = int(max(current))
+                break
+
+            elif number % 2 == 0:
+                number /= 2
+                current.add(number)
+
+            else:
+                number = (number * 3) + 1
+                current.add(int(number))
+        if begin == 1:
+            dict_max[1] = 0
+        else:
+            dict_max[begin] = max_num
+        begin += 1
+    return dict_max
+
+
+def longest(start, stop):
+    begin = start
+    dict_length = {1: 0}
+    while begin <= stop:
+        number = begin
+        numbers = set()
+        while number > 1:
+            if number % 2 == 0:
+                number /= 2
+                numbers.add(int(number))
+            else:
+                number = (number * 3) + 1
+                numbers.add(int(number))
+        dict_length[begin] = len(numbers)
+        begin += 1
+    return dict_length
+
+
+def combined(dict_length, dict_max):
+    final_dict = {}
+    for key in (dict_length.keys() | dict_max.keys()):
+        if key in dict_length: final_dict.setdefault(key, []).append(dict_length[key])
+        if key in dict_max: final_dict.setdefault(key, []).append(dict_max[key])
+    return final_dict
+
+
+start = int(input("Enter the number to start at "))
+stop = int(input("Enter the number to end at "))
+
+print(combined(longest(start, stop), highest(start, stop)))
+
+
